11bf5c27cf518b83ad6477c7af3ace190eec7ea9
MADROCHA <madlarocha@gmail.com>
2022-06-09 09:28:34 +0200

    components_jsx_card_footer_navbar_label

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-
 const Footer = () => {
-    return (
-
-        <div className="flex flex-row bg-blueSet-300 text-white w-72 h-72  text-3xl font-source ">
-
-            <div class=" bg-orangeF w-full h-full ">
-                
-            </div>
-
-            <div class="text-6xl text-blueSet-100 ">
-                Footer
-
-            </div>
-
-
-
-
-        </div>
-    )
-}
-
-export default Footer
-
-if (document.getElementById('footer')) {
-    ReactDOM.render(<Footer />, document.getElementById('footer'));
-}
+        return (
+        <footer class="p-4 bg-setBlue-300  shadow md:flex md:items-center md:justify-between md:p-6 ">
+        <span class="text-sm text-setGray-100 sm:text-center ">© 2022
+        <a href="" class="hover:underline">Factozon™</a>. All Rights Reserved.
+        </span>
+        <ul class="flex flex-wrap text-setGray-100 items-center mt-3 text-sm sm:mt-0 ">
+        <li>
+            <a href="" class=" hover:underline mr-7  ">
+            <img class="h-10 mr-5 " src={('https://i.postimg.cc/SXnkNYs2/Facebook.png')} alt=""/>
+            </a>
+        </li>
+        <li>
+            <a href="" class=" hover:underline mr-7    ">
+            <img class="h-10 mr-5 " src={('https://i.postimg.cc/LqQ5xMqf/Instragram.png')} alt=""/>
+            </a>
+        </li>
+        <li>
+            <a href="" class=" hover:underline mr-7    ">
+            <img class="h-10 mr-5 " src={('https://i.postimg.cc/t18y1RZr/Twitter.png')} alt=""/>
+            </a>
+        </li>
+        </ul>
+        <ul class="flex flex-wrap text-setGray-100 items-center mt-3 text-sm sm:mt-0 ">
+        <li>
+            <a href="" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="" class="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+            <a href="" class="hover:underline">Contact</a>
+        </li>
+        </ul>
+        </footer>
+        )
+    }
+    export default Footer
+    if (document.getElementById('footer')) {
+        ReactDOM.render(<Footer />, document.getElementById('footer'));
+    }
--- resources/js/components/Label.jsx
+++ resources/js/components/Label.jsx
@@ -3,22 +3,42 @@ import ReactDOM from 'react-dom';
 
 const Label = () => {
     return (
+    <div>
 
-        <div className="flex flex-row bg-blueSet-300 text-white w-3/12 h-1/12  text-3xl font-source ">
 
-            <div class=" bg-orangeF w-1/12 h-full ">
+        <a href="">
+        <div class="flex flex-row bg-setBlue-100 text-setGray-100 h-14 w-52 rounded-xl overflow-hidden text-3xl font-source shadow-md  ">
+
+            <div class=" bg-setGray-200 hover:bg-setOrange w-2/12 h-full mr-1 ">
                 
             </div>
 
-            <div class="text-6xl text-blueSet-100">
-                Label Figma
 
+            <div class="flex self-center text-2xl font-bold text-setGray-100 hover:text-setOrange ">
+                Label Figma
+            </div>
+
+        </div>
+        </a>
+
+        
+
+
+                <a href="">
+        <div class="flex flex-col-reverse bg-setBlue-100 text-white h-16 w-52 rounded-xl overflow-hidden text-3xl font-source shadow-md  ">
+
+            <div class=" bg-setGray-200 hover:bg-setOrange w-full h-3 mr-1 ">
+                
             </div>
 
 
-
+            <div class="flex place-self-center mb-1 text-2xl font-bold text-setGray-100 hover:text-setOrange ">
+                Label Figma
+            </div>
 
         </div>
+        </a>
+    </div>
     )
 }
 
