35043f6dc61d1b1f8d4340c1f66dae735125a725
MADROCHA <madlarocha@gmail.com>
2022-06-13 14:55:38 +0200

    commits_from_mad1_to_mad2

--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div id="banner" data={{ $shop }} ></div>
-    <div class="py-5 container container mx-auto md:pl-[8rem] md:pr-[8rem]">
+    <div class="py-5 container mx-auto md:pl-[8rem] md:pr-[8rem]">
         <div id="products" products={{ $shop->product }} categories={{ $shop->category }} ></div>
     </div> 
 
