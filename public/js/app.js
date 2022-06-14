ee9af8015f9e2d2d64499dac6d06e55b41d836b7
jhonv4sq <jhonv4sq.01@gmail.com>
2022-06-14 15:28:17 +0200

    index products

--- public/js/app.js
+++ public/js/app.js
@@ -5402,6 +5402,8 @@ __webpack_require__(/*! ./components/shop/Banner */ "./resources/js/components/s
 
 __webpack_require__(/*! ./components/shop/Products */ "./resources/js/components/shop/Products.jsx");
 
+__webpack_require__(/*! ./components/form/Login */ "./resources/js/components/form/Login.jsx");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -5833,6 +5835,16 @@ var Navbar = function Navbar() {
                   children: "tiendas"
                 })
               })
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("li", {
+              className: "  border-b-4 border-setBlue-300 hover:border-setOrange",
+              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("a", {
+                href: "/products",
+                className: "pb-[1.25rem] pt-4 px-4 flex items-center",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("p", {
+                  className: "text-setGray-100 text-2xl",
+                  children: "Productos"
+                })
+              })
             })]
           })]
         }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
@@ -5869,6 +5881,38 @@ var Navbar = function Navbar() {
 
 if (document.getElementById("navbar")) {
   react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(Navbar, {}), document.getElementById("navbar"));
+}
+
+/***/ }),
+
+/***/ "./resources/js/components/form/Login.jsx":
+/*!************************************************!*\
+  !*** ./resources/js/components/form/Login.jsx ***!
+  \************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+
+
+var Login = function Login() {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+    children: "Login"
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Login);
+
+if (document.getElementById("login")) {
+  react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(Login, {}), document.getElementById("login"));
 }
 
 /***/ }),
